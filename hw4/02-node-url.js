437a7852b8ff4241f9bef573350a3a98f9f406c4
Caterina Paun <caterina.paun@gmail.com>
2021-04-30 08:07:41 -0700

    Add: starter files for homework 4

--- hw4/02-node-url.js
+++ hw4/02-node-url.js
@@ -1,20 +1,23 @@
 const http = require('http');
-const url = require('url');
-const querystring = require('querystring');
 
 const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   const routes = [
     '/attributes?hello=world&lorem=ipsum',
-    '/items?first=1&second=2&third=3',
+    '/items?first=1&second=2&third=3&fourth=4',
+    '/characters?spongebob=squarepants&patrick=star&sandy=cheeks',
   ];
+
+  // use the URL interface to work with URLs
+  // source: https://developer.mozilla.org/en-US/docs/Web/API/URL
+  let url = new URL(req.url, `http://${req.headers.host}`);
 
   let getRoutes = () => {
     let result = '';
 
     routes.forEach(
-      (elem) => (result += `<li><a href="/${elem}">${elem}</a></li>`)
+      (elem) => (result += `<li><a href="${elem}">${elem}</a></li>`)
     );
 
     return result;
---
+++ hw4/06-templates.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Use Pug as the templating engine
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+// REST Countries URL
+const url = 'https://restcountries.eu/rest/v2/all';
+
+// Add your code here
+
+app.get('/', (req, res) => {
+  // render pug template for the index.html file
+
+  results = [
+    'Countries and Capitals',
+    'Most Populous Countries',
+    'Regions of the World',
+  ];
+
+  res.render('page', {
+    heading: 'Countries of the World',
+    results: results,
+  });
+});
+
+app.get('/capitals', (req, res) => {
+  // map the output array to create an array with country names and capitals
+  // check for empty data in the output array
+
+  results = ['Afghanistan', 'Aland Islands', 'Albania'];
+
+  res.render('page', {
+    heading: 'Countries and Capitals',
+    results: results,
+  });
+});
+
+app.get('/populous', (req, res) => {
+  // filter the output array for the countries with population of 5ß0 million or more
+  // sort the resulting array to show the results in order of population
+  // map the resulting array into a new array with the country name and formatted population
+
+  results = ['China', 'India', 'United States of America'];
+
+  res.render('page', {
+    heading: 'Most Populous Countries',
+    results: results,
+  });
+});
+
+app.get('/regions', (req, res) => {
+  // reduce the output array in a resulting object that will feature the numbers of countries in each region
+  // disregard empty data from the output array
+
+  results = ['Asia - 50', 'Europe - 53', 'Africa - 60'];
+
+  res.render('page', {
+    heading: 'Regions of the World',
+    results: results,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
