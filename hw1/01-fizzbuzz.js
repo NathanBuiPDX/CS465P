20ecc690ca319c5f58b7acaa33841d6c0d1b1fe8
Caterina Paun <caterina.paun@gmail.com>
2022-09-29 10:25:00 -0700

    Update: starter files for homework 1

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -1,3 +1,14 @@
+/** Exercise 01 - Fizzbuzz
+
+Write a program that writes all the numbers from 1 to 100, with some exceptions: 
+- For numbers divisible by 3, print “fizz” 
+- For numbers divisible by 5 (but not 3), print “buzz” 
+- For numbers divisible by 3 and 5, print “fizzbuzz”
+
+Use console.log() to write the proper output to the command line.
+
+**/
+
 const fizzbuzz = () => {
   // Add your code here
 };
