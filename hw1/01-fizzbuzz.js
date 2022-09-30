2ac1c1a27e21ab0688a7fef844c3f9c857b413b3
huutribui <huu-tri.bui@intel.com>
2022-09-29 21:35:20 -0700

    Modified the code with the Prettier

--- hw1/01-fizzbuzz.js
+++ hw1/01-fizzbuzz.js
@@ -10,7 +10,7 @@ Use console.log() to write the proper output to the command line.
 **/
 
 const fizzbuzz = () => {
-  for (let i = 1; i < 101; i++) {
+	for (let i = 1; i < 101; i++) {
 		if (i % 5 === 0 && i % 3 === 0) console.log('FizzBuzz');
 		else if (i % 3 === 0) console.log('Fizz');
 		else if (i % 5 === 0) console.log('Buzz');
