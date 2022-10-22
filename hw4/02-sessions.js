fba07f320e6512c48359dd0800b50c7c6b4d648c
huutribui <huu-tri.bui@intel.com>
2022-10-21 19:21:39 -0700

    formatted files with prettier

--- hw4/02-sessions.js
+++ hw4/02-sessions.js
@@ -6,21 +6,22 @@ const port = process.env.PORT || 5001;
 // Add your code here
 
 // Use the express-session module
-app.use(session({
-    secret: "123456789",
-    saveUninitialized:true,
-    cookie: { maxAge: 86400 },
-    resave: false 
-}));
+app.use(
+	session({
+		secret: '123456789',
+		saveUninitialized: true,
+		cookie: { maxAge: 86400 },
+		resave: false,
+	})
+);
 app.use((req, res, next) => {
 	res.status(200);
-	res.header({"Content-Type": "text/html"});
+	res.header({ 'Content-Type': 'text/html' });
 	res.write(`<p>Currently on route:${req.url}</p>`);
 	if (!req.session.routes) {
 		req.session.routes = [`<ul><p>Previously visited:</p></ul>`];
-	}
-	else{
-		req.session.routes.forEach(route => res.write(route));
+	} else {
+		req.session.routes.forEach((route) => res.write(route));
 		req.session.routes.push(`<li>${req.url}</li>`);
 	}
 	next();
@@ -40,7 +41,6 @@ app.get('/spongebob', (req, res) => {
 	res.end();
 });
 
-
 app.get('/patrick', (req, res) => {
 	res.end();
 });
@@ -48,7 +48,6 @@ app.get('/patrick', (req, res) => {
 app.get('/sandy', (req, res) => {
 	res.end();
 });
-
 
 app.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}`);
